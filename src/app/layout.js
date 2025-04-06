b4f8135ba6d3d57f33aea7d5f0b9ff76ab0e20e8
Ronnel Mwiya <mwiyamwangajnr@gmail.com>
2025-04-06 18:50:40 +0200

    Added components and pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { UserProvider } from './context/userContext';
+import { UserProvider } from '../context/userContext';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
