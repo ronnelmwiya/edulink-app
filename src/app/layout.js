904f898e724fca47af5dd9742734d9c4ac339067
Ronnel Mwiya <mwiyamwangajnr@gmail.com>
2025-04-06 22:41:29 +0200

    Added dashboard css and fixed layout code errors.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,5 @@
 import { UserProvider } from '../context/userContext';
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Solace",
@@ -20,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className="font-roboto antialiased">
         <UserProvider>
           {children}
         </UserProvider>
